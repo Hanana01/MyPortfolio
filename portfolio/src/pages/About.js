6410811fa9cc5f08e41f0b3f72eac940833e1783
FathiHanana <hananaf2001@gmail.com>
2025-03-03 12:38:46 +0530

    modified

---
+++ portfolio/src/pages/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/About.css"; 
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>This is where you describe yourself.</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ portfolio/src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Contact.css"; 
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>Contact Me</h2>
+      <p>Feel free to reach out!</p>
+    </div>
+  );
+};
+
+export default Contact;
